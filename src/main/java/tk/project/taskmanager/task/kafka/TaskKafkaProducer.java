6541d1d0fd55b8e3d51719018346dcaa91799479
Constantin <ktimonin4@gmail.com>
2025-04-13 19:43:57 +0400

    Add logging starter

--- src/main/java/tk/project/taskmanager/task/kafka/TaskKafkaProducer.java
+++ src/main/java/tk/project/taskmanager/task/kafka/TaskKafkaProducer.java
@@ -1,22 +1,28 @@
 package tk.project.taskmanager.task.kafka;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import tk.project.taskmanager.aspect.annotation.LogCoverMethod;
 import tk.project.taskmanager.kafka.KafkaProducer;
+import tk.project.taskmanager.logging.aspect.annotation.LogCoverMethod;
 import tk.project.taskmanager.task.kafka.dto.ChangeTaskStatusDto;
 import tk.project.taskmanager.task.kafka.dto.ChangeTaskStatusEvent;
 import tk.project.taskmanager.task.model.TaskStatus;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Component
-@RequiredArgsConstructor
 public class TaskKafkaProducer {
     private final KafkaProducer kafkaProducer;
 
+    public TaskKafkaProducer(@Autowired(required = false) KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
     @LogCoverMethod
     public void sendMsgChangeTaskStatus(UUID taskId, TaskStatus status) {
+        if (Objects.isNull(kafkaProducer)) return;
+
         ChangeTaskStatusDto dto = new ChangeTaskStatusDto(taskId, status);
         ChangeTaskStatusEvent event = new ChangeTaskStatusEvent();
         event.setChangeTaskStatusDto(dto);
